6e8ec0238433e226f82442f11051b97dfba33da5
Adam Botma <adam.botma@me.com>
2020-05-07 15:21:29 -0700

    cleaned up code

--- components/grades-form.js
+++ components/grades-form.js
@@ -1,28 +1,22 @@
 class GradeForm {
+
   constructor(formElement){
     this.formElement = formElement;
     this.handleSubmit = this.handleSubmit.bind(this);
     this.eventListener = document.addEventListener('submit', this.handleSubmit);
-
   }
 
-onSubmit (createGrade){
-  this.createGrade = createGrade;
-}
+  onSubmit (createGrade){
+    this.createGrade = createGrade;
+  }
 
-handleSubmit(event){
-  event.preventDefault();
-  var formData = new FormData(event.target);
-  var currentName = formData.get('name');
-  var currentCourse = formData.get('course');
-  var currentGrade = formData.get('grade');
-  this.createGrade(currentName, currentCourse, currentGrade);
-  event.target.reset();
-
-}
-
-
-
-
-
+  handleSubmit(event){
+    event.preventDefault();
+    var formData = new FormData(event.target);
+    var currentName = formData.get('name');
+    var currentCourse = formData.get('course');
+    var currentGrade = formData.get('grade');
+    this.createGrade(currentName, currentCourse, currentGrade);
+    event.target.reset();
+  }
 }
--- main.js
+++ main.js
@@ -2,13 +2,8 @@ var myTable = document.querySelector('table');
 var myHeader = document.querySelector('header');
 var myForm = document.querySelector('form');
 var noGrades = document.getElementById('no-grades')
-
 var gradeTable = new GradeTable(myTable, noGrades);
-
 var pageHeader = new PageHeader(myHeader);
-
-
 var gradeForm = new GradeForm(myForm);
-
 var app = new App(gradeTable, pageHeader, gradeForm);
 app.start();
